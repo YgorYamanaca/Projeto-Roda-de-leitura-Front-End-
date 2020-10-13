cdb29efbb639007aff53d26ab7ce473a4a096145
DaniloFerraz <daniloferraz97@gmail.com>
2020-10-12 21:45:39 -0300

    Rota chat

---
+++ src/components/Chat/index.js
@@ -0,0 +1,31 @@
+import React, { memo } from 'react';
+import {Title} from './styles'
+import {HeaderText} from './styles'
+import {Header} from './styles'
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/ImportContacts';
+import { Book } from '@material-ui/icons';
+import { shadows } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
+
+
+function Chat() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat odio ut magna dictum bibendum. Donec vestibulum placerat ante ut suscipit. Suspendisse potenti. Morbi in porttitor sapien, in scelerisque urna. Proin aliquam nisi quis lacus tincidunt commodo. Fusce accumsan condimentum dui. Nunc ac lectus quis odio sollicitudin aliquam.
+
+        Praesent quis luctus nibh. Aliquam ultrices, neque quis malesuada sollicitudin, massa nulla vestibulum arcu, sit amet pellentesque dolor purus a lorem. Praesent tellus diam, feugiat eget dignissim vel, molestie lobortis nulla. Mauris molestie laoreet ex, ac consectetur nisl aliquam et. Donec in diam interdum, tempus dolor quis, lobortis lorem. Etiam consectetur viverra lectus, at sollicitudin arcu venenatis at. Morbi at facilisis massa. Quisque eleifend ultrices tortor, viverra rhoncus ex condimentum vulputate. Nunc accumsan interdum pellentesque. Fusce sagittis aliquet efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        
+        Fusce vestibulum lacinia turpis sit amet sagittis. Donec et lorem a lacus euismod efficitur. Suspendisse eget interdum urna, ut faucibus diam. Nunc laoreet, justo feugiat sollicitudin ornare, est dolor consectetur mi, in fermentum elit diam sed leo. Morbi non interdum odio, ac accumsan mauris. Integer egestas diam ut odio egestas gravida. Phasellus in lorem consectetur, pulvinar dolor sit amet, interdum libero. Nullam tempus, metus quis sodales ultrices, ligula diam ultrices nulla, sed ultricies purus sapien id metus. Ut vel tincidunt sapien. Phasellus ullamcorper justo rutrum lacus posuere, tincidunt vulputate risus suscipit. Duis eleifend urna sed felis placerat, sed suscipit turpis luctus.
+        
+        Pellentesque at metus lobortis, tincidunt turpis a, fringilla ligula. Praesent vehicula lectus ligula, vel feugiat mi cursus non. Ut feugiat id lectus id commodo. Cras convallis purus ut turpis bibendum, vel porttitor enim molestie. Quisque risus enim, pretium et sagittis quis, vestibulum id lacus. Morbi maximus nisi sit amet laoreet gravida. Etiam viverra enim vitae nunc gravida vestibulum. Curabitur mollis gravida commodo. In eu tincidunt magna. Proin sodales tempor scelerisque. Maecenas blandit lorem vel posuere euismod. Fusce eget pretium velit. Integer quis tempus justo, ac lobortis nisl. Suspendisse potenti. Ut ornare quam erat, in semper libero facilisis non. Nulla vel volutpat dolor, in feugiat odio.
+        
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent sit amet suscipit sapien. Proin consequat lorem at pharetra ornare. Phasellus nec nibh in mauris laoreet malesuada in sit amet erat. Sed posuere quam enim, et rhoncus ex aliquet id. Donec purus sapien, blandit id vehicula ut, imperdiet ut neque. Phasellus et risus quam. Suspendisse non vehicula quam.</div> 
+    );
+  }
+  
+export default Chat;
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserComponent from './components/UserComponent';
 import AllEvents from './components/AllEvents'
 import Calender from './components/Calender';
 import Forum from './components/Forum'
+import Chat from './components/Chat'
 import { isAuthenticated } from "./services/auth";
 import  {Provider}  from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -37,10 +38,11 @@ export default function Routes(){
                     <Route exact path="/signup" component={SignUp}/>
 
                     <Home>
-                        <PrivateRoute exact path="/user" component={UserComponent}/>
-                        <PrivateRoute exact path="/calender" component={Calender}/>
-                        <PrivateRoute exact path="/events" component={AllEvents}/>
-                        <PrivateRoute exact path="/forum" component={Forum}/>
+                        <Route exact path="/user" component={UserComponent}/>
+                        <Route exact path="/calender" component={Calender}/>
+                        <Route exact path="/events" component={AllEvents}/>
+                        <Route exact path="/forum" component={Forum}/>
+                        <Route exact path="/chat" component={Chat}/>
                     </Home>
                     <Redirect from="*" to="/" />
                 </Switch>
